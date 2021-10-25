d634794a5761c79aef0447693abd9da547e54112
adrianomonteiroweb <adrianomonteiroweb@gmail.com>
2021-10-25 19:18:59 -0300

    desafio 1

--- desafio1.sql
+++ desafio1.sql
@@ -1,5 +1,5 @@
 SELECT
-COUNTRY_NAME AS País,
-IF(REGION_ID = 1, 'incluído', 'não incluído') AS 'Status Inclusão'
+country_name AS País,
+IF(region_id = (SELECT region_ID FROM hr.regions WHERE region_name = 'Europe'), 'incluído', 'não incluído') AS 'Status Inclusão'
 FROM hr.countries
 ORDER BY País;
