73e49c642c78dc3fbcaf2d58361c2a2b001189e3
adrianomonteiroweb <adrianomonteiroweb@gmail.com>
2021-10-26 20:58:56 -0300

    resafio 7

---
+++ desafio7.sql
@@ -0,0 +1,10 @@
+SELECT 
+UCASE(CONCAT(emp.first_name, ' ', emp.last_name)) AS `Nome completo`,
+jobhis.start_date AS `Data de início`,
+emp.salary AS `Salário`
+FROM
+hr.job_history AS jobhis
+INNER JOIN
+hr.employees AS emp ON jobhis.employee_id = emp.employee_id
+WHERE MONTH(jobhis.start_date) IN (01,02,03)
+ORDER BY `Nome completo` ASC, Salário ASC;
