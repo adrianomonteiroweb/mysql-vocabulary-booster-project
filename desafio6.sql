1ceb958074ce59f47d36b43bc1a72df2c710b8f0
adrianomonteiroweb <adrianomonteiroweb@gmail.com>
2021-10-26 15:25:44 -0300

    desafio 6

--- desafio6.sql
+++ desafio6.sql
@@ -3,9 +3,12 @@ CONCAT(first_name, ' ', last_name) AS 'Nome completo',
 (SELECT job.job_title 
 FROM hr.jobs AS job 
 WHERE job.job_id = emp.job_id) AS Cargo,
-hire_date AS 'Data de início do cargo',
+job_his.start_date AS 'Data de início do cargo',
 (SELECT department_name
 FROM hr.departments AS dep
 WHERE dep.DEPARTMENT_ID = emp.DEPARTMENT_ID) AS Departamento
-FROM hr.employees AS emp
+FROM
+hr.employees AS emp
+JOIN
+hr.job_history AS job_his
 ORDER BY `Nome completo`DESC, Cargo ASC;
