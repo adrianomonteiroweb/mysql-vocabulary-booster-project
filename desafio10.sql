8c1aa6feafe2bd2a2408d9a38510ce414984f6c8
adrianomonteiroweb <adrianomonteiroweb@gmail.com>
2021-10-26 22:39:36 -0300

    desafio 10

---
+++ desafio10.sql
@@ -0,0 +1,11 @@
+SELECT 
+pro.productname AS `Produto`,
+MIN(orddet.quantity) AS `Mínima`,
+MAX(orddet.quantity) AS `Máxima`,
+ROUND(AVG(orddet.Quantity), 2) AS `Média`
+FROM
+w3schools.products AS pro
+INNER JOIN
+w3schools.order_details AS orddet ON pro.productid = orddet.productid
+GROUP BY Produto HAVING Média > 20 
+ORDER BY Média ASC, Produto ASC;
