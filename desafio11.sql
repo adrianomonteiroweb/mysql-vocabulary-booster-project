2602de5c472db563c1348daf42d8cb01b6575a87
adrianomonteiroweb <adrianomonteiroweb@gmail.com>
2021-10-27 10:01:06 -0300

    desafios 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,8 +1,8 @@
 SELECT 
-cus1.ContactName AS `Nome`,
-cus1.Country AS `País`,
+cus1.contactName AS `Nome`,
+cus1.country AS `País`,
 (SELECT COUNT(*) - 1
-FROM w3schools.customers AS c2
+FROM w3schools.customers AS cus2
 WHERE cus1.country = cus2.country
 GROUP BY cus2.country) AS `Número de compatriotas`
 FROM w3schools.customers AS cus1
