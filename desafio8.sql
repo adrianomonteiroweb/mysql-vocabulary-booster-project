6687a743585af3bddbcf2b8b9c7e0d830ed1b963
adrianomonteiroweb <adrianomonteiroweb@gmail.com>
2021-10-26 21:15:51 -0300

    resafio 8

---
+++ desafio8.sql
@@ -0,0 +1,13 @@
+SELECT 
+cus.contactname AS `Nome de contato`,
+shi.shippername AS `Empresa que fez o envio`,
+ord.orderdate AS `Data do pedido`
+FROM
+w3schools.customers AS cus
+INNER JOIN
+w3schools.orders AS ord ON cus.customerid = ord.customerid
+INNER JOIN
+w3schools.shippers AS shi ON ord.shipperid = shi.shipperid
+WHERE
+shi.shipperid IN (1 , 2)
+ORDER BY `Nome de contato` ASC, `Empresa que fez o envio` ASC, `Data do pedido` ASC;
